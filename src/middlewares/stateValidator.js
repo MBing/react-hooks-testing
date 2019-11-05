096fff77f386a7630de42c320a766433826cea2c
Martin Demiddel <martin.demiddel@quandoo.com>
2019-11-05 20:12:28 +0100

    fix(validator): add warn message

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -5,5 +5,7 @@ import { stateSchema } from './stateSchema';
 export default ({ dispatch, getState }) => next => action => {
     next(action);
 
-    tv4.validate(getState(), stateSchema);
+    if (tv4.validate(getState(), stateSchema)) {
+        console.warn('Invalid State Schema Detected!');
+    }
 };
